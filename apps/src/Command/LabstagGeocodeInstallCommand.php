6cf057a15138aa21d09a906730fbdffd5fde503c
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2024-12-06 10:01:17 +0000

    Add geocoding functionality with command, service, entity, and repository

---
+++ apps/src/Command/LabstagGeocodeInstallCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Labstag\Command;
+
+use Exception;
+use Labstag\Repository\GeoCodeRepository;
+use Labstag\Service\GeocodeService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'labstag:geocode:install',
+    description: 'Retrieve geocodes',
+)]
+class LabstagGeocodeInstallCommand extends Command
+{
+    public function __construct(
+        private GeocodeService $geocodeService,
+        private GeoCodeRepository $geoCodeRepository
+    )
+    {
+        
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('country', InputArgument::REQUIRED, 'country code');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $symfonyStyle = new SymfonyStyle($input, $output);
+        $symfonyStyle->title('Retrieving postal codes');
+
+        $country = $input->getArgument('country');
+        if (!is_string($country)) {
+            throw new Exception('Argument country invalide');
+        }
+
+        if (empty($country)) {
+            $symfonyStyle->note(
+                sprintf(
+                    'Argument countrie obligatoire: %s',
+                    $country
+                )
+            );
+
+            return COMMAND::FAILURE;
+        }
+
+        $csv = $this->geocodeService->csv($country);
+        if ([] == $csv) {
+            $symfonyStyle->warning(
+                ['fichier inexistant']
+            );
+
+            return COMMAND::FAILURE;
+        }
+
+        $progressBar = new ProgressBar($output, is_countable($csv) ? count($csv) : 0);
+        $table       = $this->geocodeService->tables($csv);
+        $progressBar->start();
+        $counter = 0;
+        foreach ($table as $row) {
+            $entity = $this->geocodeService->add($row);
+            $this->geoCodeRepository->persist($entity);
+            $counter++;
+            $this->geoCodeRepository->flush($counter);
+            $progressBar->advance();
+        }
+
+        $this->geoCodeRepository->flush();
+
+        $progressBar->finish();
+        $symfonyStyle->newLine();
+        $symfonyStyle->success("Fin d'enregistrement");
+
+        return Command::SUCCESS;
+    }
+}
---
+++ apps/src/Service/GeocodeService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Labstag\Service;
+
+use Labstag\Entity\GeoCode;
+use Labstag\Repository\GeoCodeRepository;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use ZipArchive;
+
+class GeocodeService
+{
+    /**
+     * @var int
+     */
+    final public const HTTP_OK = 200;
+
+    public function __construct(
+        protected HttpClientInterface $httpClient,
+        protected GeoCodeRepository $geoCodeRepository
+    )
+    {
+    }
+
+    public function add(array $row): GeoCode
+    {
+        $entity = $this->geoCodeRepository->findOneBy(
+            [
+                'countryCode' => $row[0],
+                'postalCode'  => $row[1],
+                'placeName'   => $row[2],
+            ]
+        );
+        if (!($entity instanceof GeoCode)) {
+            $entity = new GeoCode();
+            $entity->setCountryCode($row[0]);
+            $entity->setPostalCode($row[1]);
+            $entity->setPlaceName($row[2]);
+        }
+
+        $entity->setStateName($row[3]);
+        $entity->setStateCode($row[4]);
+        $entity->setProvinceName($row[5]);
+        $entity->setProvinceCode($row[6]);
+        $entity->setCommunityName($row[7]);
+        $entity->setCommunityCode($row[8]);
+        $entity->setLatitude($row[9]);
+        $entity->setLongitude($row[10]);
+        $entity->setAccuracy((int) $row[11]);
+
+        return $entity;
+    }
+
+    public function csv(string $country): array
+    {
+        $country  = strtoupper($country);
+        $file     = 'http://download.geonames.org/export/zip/'.$country.'.zip';
+        $response = $this->httpClient->request(
+            'GET',
+            $file
+        );
+        $statusCode = $response->getStatusCode();
+        if (self::HTTP_OK != $statusCode) {
+            return [];
+        }
+
+        $content = $response->getContent();
+        /** @var resource $tempFile */
+        $tempFile = tmpfile();
+        $path     = stream_get_meta_data($tempFile)['uri'];
+        file_put_contents($path, $content);
+        $zipArchive = new ZipArchive();
+        if (!$zipArchive->open($path)) {
+            return [];
+        }
+
+        $content = (string) $zipArchive->getFromName($country.'.txt');
+        $csv     = str_getcsv($content, "\n");
+        $zipArchive->close();
+
+        return $csv;
+    }
+
+    public function tables(array $csv): array
+    {
+        $data = [];
+        foreach ($csv as $line) {
+            $row    = str_getcsv((string) $line, "\t");
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+}
