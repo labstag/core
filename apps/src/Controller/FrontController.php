642a4d077a308957e3d17c8a8ac6f05db2216d96
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-01-05 19:13:09 +0000

    Add Movie entity and repository; enhance sitemap functionality; update admin controllers and templates for movie management

--- apps/src/Controller/FrontController.php
+++ apps/src/Controller/FrontController.php
@@ -2,10 +2,13 @@
 
 namespace Labstag\Controller;
 
+use Labstag\Service\SitemapService;
 use Labstag\Service\SiteService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[Route('/')]
 class FrontController extends AbstractController
@@ -27,6 +30,76 @@ class FrontController extends AbstractController
         return $this->render(
             $siteService->getViewByEntity($entity),
             $siteService->getDataByEntity($entity)
+        );
+    }
+
+    #[Route('sitemap.css', name: 'sitemap.css', priority: 1)]
+    public function sitemapCss()
+    {
+        $response = new Response(
+            $this->renderView('sitemap/sitemap.css.twig'),
+            200
+        );
+        $response->headers->set('Content-Type', 'text/css');
+
+        return $response;
+    }
+
+    #[Route('sitemap.js', name: 'sitemap.js', priority: 1)]
+    public function sitemapJs()
+    {
+        $response = new Response(
+            $this->renderView('sitemap/sitemap.js.twig'),
+            200
+        );
+        $response->headers->set('Content-Type', 'text/javascript');
+
+        return $response;
+    }
+
+    #[Route('sitemap.xml', name: 'sitemap.xml', priority: 1, defaults: ['_format' => 'xml'])]
+    public function sitemapXml(
+        SitemapService $sitemapService
+    )
+    {
+        return $this->initCache()->get(
+            'sitemap.xml',
+            function (ItemInterface $item) use ($sitemapService)
+            {
+                $item->expiresAfter(3600);
+
+                $sitemap = $sitemapService->getData(1);
+                dd($sitemap);
+
+                return $this->render(
+                    'sitemap/sitemap.xml.twig',
+                    [
+                        'date'    => date('Y-m-d'),
+                        'sitemap' => $sitemap,
+                    ]
+                );
+            }
+        );
+    }
+
+    #[Route('sitemap.xsl', name: 'sitemap.xsl', priority: 1)]
+    public function sitemapXsl()
+    {
+        $response = new Response(
+            $this->renderView('sitemap/sitemap.xsl.twig'),
+            200
+        );
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
+    }
+
+    protected function initCache()
+    {
+        return new FilesystemAdapter(
+            'cache.app',
+            0,
+            '../var'
         );
     }
 }
---
+++ apps/src/Entity/Movie.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Labstag\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Labstag\Repository\MovieRepository;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    use SoftDeleteableEntity;
+    use TimestampableEntity;
+
+    /**
+     * @var Collection<int, Category>
+     */
+    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'movies', cascade: ['persist', 'detach'])]
+    private Collection $categories;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 1])]
+    private ?bool $enable = null;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\Column(type: 'guid', unique: true)]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imdb = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $year = null;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->addMovie($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getImdb(): ?string
+    {
+        return $this->imdb;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function isEnable(): ?bool
+    {
+        return $this->enable;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeMovie($this);
+        }
+
+        return $this;
+    }
+
+    public function setCountry(?string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function setEnable(bool $enable): static
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+
+    public function setImdb(?string $imdb): static
+    {
+        $this->imdb = $imdb;
+
+        return $this;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function setYear(?int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
