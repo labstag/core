e74f66970d569e1b9b7028cd96f02aa0324071e0
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-01-06 15:06:49 +0000

    Skip specific Rector rules for improved configuration flexibility

--- apps/rector.php
+++ apps/rector.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 
 $configure = RectorConfig::configure();
 $configure->withPaths([
@@ -34,5 +37,12 @@ $configure->withPreparedSets(
     doctrineCodeQuality: true,
     symfonyCodeQuality: true,
     symfonyConfigs: true
+);
+$configure->withSkip(
+    [
+        ReadOnlyPropertyRector::class,
+        AddTypeToConstRector::class,
+        ReadOnlyClassRector::class
+    ]
 );
 return $configure;
