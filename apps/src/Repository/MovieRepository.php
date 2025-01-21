7325b9b63b88a3158f4fd81da5ef709b5754664e
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-01-21 13:54:10 +0000

    Add MovieParagraph class and related styles; refactor StarParagraph and MovieRepository

--- apps/src/Repository/MovieRepository.php
+++ apps/src/Repository/MovieRepository.php
@@ -2,6 +2,8 @@
 
 namespace Labstag\Repository;
 
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Labstag\Entity\Movie;
 use Labstag\Lib\ServiceEntityRepositoryLib;
@@ -11,5 +13,21 @@ class MovieRepository extends ServiceEntityRepositoryLib
     public function __construct(ManagerRegistry $managerRegistry)
     {
         parent::__construct($managerRegistry, Movie::class);
+    }
+
+    public function getQueryBuilder(): QueryBuilder
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->where('s.enable = :enable');
+        $queryBuilder->setParameter('enable', true);
+
+        return $queryBuilder->orderBy('s.title', 'ASC');
+    }
+
+    public function getQueryPaginator(): Query
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        return $queryBuilder->getQuery();
     }
 }
