0b30d44207f21e500dc080056c5253e1e1faaa43
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-02-04 15:30:48 +0000

    Enhance accessibility by adding area-label attributes to links; update header styles for better visibility; introduce new SCSS file for page teasers; implement movie description handling in MovieService and EntityListener

--- apps/src/Command/MoviesUpdateCommand.php
+++ apps/src/Command/MoviesUpdateCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'labstag:movies-update', description: 'Add a short description for your command',)]
+#[AsCommand(name: 'labstag:movies-update', description: 'Update movie description and image',)]
 class MoviesUpdateCommand extends Command
 {
     public function __construct(
@@ -24,14 +24,17 @@ class MoviesUpdateCommand extends Command
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $symfonyStyle = new SymfonyStyle($input, $output);
+        $this->updateImage($output, $symfonyStyle);
+        $this->updateDescription($output, $symfonyStyle);
 
+        return Command::SUCCESS;
+    }
+
+    private function updateImage($output, $symfonyStyle)
+    {
         // Movie without img
         $movies = $this->movieRepository->findBy(
             ['img' => null]
@@ -43,7 +46,7 @@ class MoviesUpdateCommand extends Command
         $update  = 0;
         $counter = 0;
         foreach ($movies as $movie) {
-            $status = $this->movieService->update($movie);
+            $status = $this->movieService->updateImage($movie);
             $update = $status ? ++$update : $update;
             ++$counter;
 
@@ -56,8 +59,35 @@ class MoviesUpdateCommand extends Command
         $progressBar->finish();
 
         $numberFormatter = new NumberFormatter('fr_FR', NumberFormatter::DECIMAL);
-        $symfonyStyle->success(sprintf('Updated: %d', $numberFormatter->format($update)));
+        $symfonyStyle->success(sprintf('Images updated: %d', $numberFormatter->format($update)));
+    }
 
-        return Command::SUCCESS;
+    private function updateDescription($output, $symfonyStyle)
+    {
+        // Movie without img
+        $movies = $this->movieRepository->findBy(
+            ['description' => null]
+        );
+
+        $progressBar = new ProgressBar($output, count($movies));
+        $progressBar->start();
+
+        $update  = 0;
+        $counter = 0;
+        foreach ($movies as $movie) {
+            $status = $this->movieService->updateDescription($movie);
+            $update = $status ? ++$update : $update;
+            ++$counter;
+
+            $this->movieRepository->persist($movie);
+            $this->movieRepository->flush($counter);
+            $progressBar->advance();
+        }
+
+        $this->movieRepository->flush();
+        $progressBar->finish();
+
+        $numberFormatter = new NumberFormatter('fr_FR', NumberFormatter::DECIMAL);
+        $symfonyStyle->success(sprintf('Descriptions updated: %d', $numberFormatter->format($update)));
     }
 }
