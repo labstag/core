1216906f1e6b8e25cbd01392d33108917b8c5c20
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-02-06 15:16:13 +0000

    Add control key detection to context menu for enhanced functionality

--- assets/front/js/contextmenu.js
+++ assets/front/js/contextmenu.js
@@ -1,11 +1,28 @@
 export class ContextMenu {
+
+  static ctrlPressed = false;
   constructor() {
     if (!document.getElementById('contextMenu')) {
       return;
     }
     const contextMenu = document.getElementById("contextMenu");
     const menuItems = document.getElementById("menuItems");
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Control') {
+            this.ctrlPressed = true;
+        }
+    });
+    
+    document.addEventListener('keyup', (event) => {
+        if (event.key === 'Control') {
+            this.ctrlPressed = false;
+        }
+    });
     document.addEventListener("contextmenu", (event) => {
+      if (!this.ctrlPressed) {
+        return;
+      }
+      
       event.preventDefault();
 
       const blockdiv = event.target.closest('.block');
@@ -16,8 +33,7 @@ export class ContextMenu {
           'text': blockdiv.dataset.context_text,
         }
       );
-      console.log(blockdiv);
-      console.log(blockdiv.querySelectorAll('.paragraph').length);
+      
       blockdiv.querySelectorAll('.paragraph').forEach((paragraphdiv) => {
         options.push(
           {
@@ -27,11 +43,7 @@ export class ContextMenu {
         )
       });
 
-      console.log(options);
-
       menuItems.innerHTML = options.map(option => `<li><a href="${option.url}" target="_blank">${option.text}</a></li>`).join("");
-
-
       contextMenu.style.display = "block";
       contextMenu.style.left = `${event.pageX}px`;
       contextMenu.style.top = `${event.pageY}px`;
