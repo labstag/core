ea725b72ad8dc7a994afbbcfc215a2082cf2dbb5
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-10-28 20:47:19 +0100

    fix: Ajouter une vérification pour s'assurer que le code IMDb est fourni

--- apps/src/Command/ImdbCommand.php
+++ apps/src/Command/ImdbCommand.php
@@ -42,6 +42,12 @@ class ImdbCommand extends Command
     {
         $symfonyStyle = new SymfonyStyle($input, $output);
         $imdb         = $symfonyStyle->ask('Quel est le code IMDb ?');
+        if (empty($imdb)) {
+            $symfonyStyle->error("Le code IMDB est obligatoire");
+
+            return Command::INVALID;
+        }
+
         $data         = $this->tmdbApi->findByImdb($imdb);
         if (is_null($data)) {
             $symfonyStyle->error("Le code IMDB n'est pas valide");
