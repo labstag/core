304a8bd6a3888a93278a7f1344165c6adbc3c9c3
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2026-01-13 15:31:11 +0100

    feat: ajouter des options pour mettre à jour les personnes, les films et les séries dans la commande d'exécution des messages

--- apps/src/Command/ExecuteMessageCommand.php
+++ apps/src/Command/ExecuteMessageCommand.php
@@ -5,8 +5,11 @@ namespace Labstag\Command;
 use Labstag\Message\BanIpMessage;
 use Labstag\Message\DeleteOldFileMessage;
 use Labstag\Message\MetaAllMessage;
+use Labstag\Message\MovieAllMessage;
 use Labstag\Message\NotificationMessage;
 use Labstag\Message\PageCinemaMessage;
+use Labstag\Message\PersonAllMessage;
+use Labstag\Message\SerieAllMessage;
 use Labstag\Message\UpdateSerieMessage;
 use Labstag\Service\MessageDispatcherService;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -30,12 +33,15 @@ class ExecuteMessageCommand
             'UpdateSerie'  => 'Update series',
             'Notification' => 'Send notifications',
             'Meta'         => 'Clean meta entries',
+            'Person'       => 'Update persons',
+            'Movie'        => 'Update movies',
+            'Serie'        => 'Update series',
             'Files'        => 'Clean files',
             'All'          => 'Execute all tasks',
             'Cancel'       => 'Cancel execution',
         ];
 
-        $selected = $symfonyStyle->choice('Which task do you want to execute?', array_values($choices), 6);
+        $selected = $symfonyStyle->choice('Which task do you want to execute?', array_values($choices), 9);
 
         $selectedKey = array_search($selected, $choices, true);
 
@@ -44,6 +50,9 @@ class ExecuteMessageCommand
             'BanIp'        => BanIpMessage::class,
             'UpdateSerie'  => UpdateSerieMessage::class,
             'Meta'         => MetaAllMessage::class,
+            'Person'       => PersonAllMessage::class,
+            'Movie'        => MovieAllMessage::class,
+            'Serie'        => SerieAllMessage::class,
             'Files'        => DeleteOldFileMessage::class,
             'Notification' => NotificationMessage::class,
         ];
